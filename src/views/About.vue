4db5bb73c8dfcbc3d7bc0fb4a93146a210e399cc
BlakeJerkins <57221035+BlakeJerkins@users.noreply.github.com>
2023-06-13 21:09:03 -0400

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,82 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="loan-calculator">
+    <h2>Loan Payoff Calculator</h2>
+
+    <!-- Input fields -->
+    <div class="input-container">
+      <label for="loan-amount">Loan Amount:</label>
+      <input id="loan-amount" type="number" v-model="loanAmount" />
+    </div>
+
+    <div class="input-container">
+      <label for="interest-rate">Interest Rate:</label>
+      <input id="interest-rate" type="number" v-model="interestRate" />
+    </div>
+
+    <div class="input-container">
+      <label for="loan-term">Loan Term (in months):</label>
+      <input id="loan-term" type="number" v-model="loanTerm" />
+    </div>
+
+    <div class="input-container">
+      <label for="down-payment">Down Payment (optional):</label>
+      <input id="down-payment" type="number" v-model="downPayment" />
+    </div>
+
+    <!-- Calculate button -->
+    <button @click="calculatePayoff" class="calculate-button">Calculate</button>
+
+    <!-- Results -->
+    <div class="results" v-if="showResults">
+      <h3>Loan Payoff Details:</h3>
+      <p>Monthly Installment: {{ monthlyInstallment }}</p>
+      <p>Total Interest Paid: {{ totalInterestPaid }}</p>
+
+      <!-- Collapsible Amortization Chart -->
+      <button @click="toggleAmortization" class="chart-toggle-button">
+        {{ showAmortization ? 'Hide Amortization Chart' : 'Show Amortization Chart' }}
+      </button>
+      <div class="amortization-chart" v-if="showAmortization">
+        <!-- Render the amortization chart here -->
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+<script>
+export default {
+  data() {
+    return {
+      loanAmount: 0,
+      interestRate: 0,
+      loanTerm: 0,
+      downPayment: 0,
+      showResults: false,
+      showAmortization: false,
+      monthlyInstallment: 0,
+      totalInterestPaid: 0,
+      amortizationData: [], // Placeholder for amortization chart data
+    };
+  },
+  methods: {
+    calculatePayoff() {
+      // Perform loan payoff calculation here
+      // Update the calculated results and amortizationData
+      this.monthlyInstallment = // Calculate the monthly installment
+      this.totalInterestPaid = // Calculate the total interest paid
 
-@Component({})
-export default class About extends Vue {
+      // Generate the amortization data
+      this.amortizationData = // Generate the amortization data based on loan details
 
-
-}
+      this.showResults = true;
+    },
+    toggleAmortization() {
+      this.showAmortization = !this.showAmortization;
+    },
+  },
+};
 </script>
+
+<style>
+/* Add your desired CSS styling for the component here */
+</style>
